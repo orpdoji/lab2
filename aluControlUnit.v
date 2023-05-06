b110b5046f0dbdcabae8c497f5c3fbbab56f3473
orpdoji <97266005+orpdoji@users.noreply.github.com>
2023-05-06 01:30:00 -0700

    codespace commit is not working so this is done manually

--- aluControlUnit.v
+++ aluControlUnit.v
@@ -1,7 +1,7 @@
 //=========================================================================
 // Name & Email must be EXACTLY as in Gradescope roster!
-// Name: 
-// Email: 
+// Name: Weijie Yuan        
+// Email: wyuan024@ucr.edu
 // 
 // Assignment name: 
 // Lab section: 
@@ -17,10 +17,28 @@ module aluControlUnit (
     input  wire [1:0] alu_op, 
     input  wire [5:0] instruction_5_0, 
     output wire [3:0] alu_out
+
     );
 
 // ------------------------------
 // Insert your solution below
 // ------------------------------ 
+reg [3:0] aluoutput;
+
+always @ (alu_op, instruction_5_0) begin
+    casex ({alu_op, instruction_5_0})
+        8'b1xxx0100: aluoutput = 4'b0000;
+        8'b1xxx0101: aluoutput = 4'b0001; 
+        8'b00xxxxxx: aluoutput = 4'b0010;
+        8'b1xxx0000: aluoutput = 4'b0010;
+        8'bx1xxxxxx: aluoutput = 4'b0110;
+        8'b1xxx0010: aluoutput = 4'b0110;
+        8'b1xxx1010: aluoutput = 4'b0111;
+        8'b1xxx0111: aluoutput = 4'b1100;
+    endcase
+end
+
+assign alu_out = aluoutput;
 
 endmodule
+
